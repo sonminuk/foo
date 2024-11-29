6287a5c990667da427ed0cff8bc55c70784f93ce
sonminuk <joysms007@gmail.com>
2024-11-29 16:12:52 +0900

    Updates

---
+++ _next/static/chunks/app/layout-d2002bcadb087363.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5619:function(e,n,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,768,23)),Promise.resolve().then(t.t.bind(t,681,23)),Promise.resolve().then(t.t.bind(t,2778,23))},2778:function(){}},function(e){e.O(0,[981,442,971,117,744],function(){return e(e.s=5619)}),_N_E=e.O()}]);
